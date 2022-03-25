7907d82070c5e425f8e3718b46c177a8c5739dcd
PyroG8r <emil.jons@live.se>
2022-03-25 16:53:47 +0100

    commit

---
+++ Objektbaserad programmering C++/Lab 2/linked_list.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "linked_list.h"
+
+
+
+linked_list::linked_list(){
+    head = nullptr;
+    tail = nullptr;
+    size_t = 
+}
+
+linked_list::node::node(double value) {
+    this->value = value;
+    next = nullptr;
+    prev = nullptr;
+}
+
+
+void linked_list::push_back(double value){
+
+}
---
+++ Objektbaserad programmering C++/Lab 2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "linked_list.h"
+#include "tests.h"
+
+int main(){
+
+}
+
+
+
+
+pop_back();
+push_front(l.pop_back());
+
+void print_list(linked_list l);
+
+linked_list merge(linked_list& ,linked_list&);
